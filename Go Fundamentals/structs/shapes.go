075cdd07bd22de29fccbedd5af0038c7e732df3a
acheampe <acheampe@oregonstate.edu>
2025-08-18 19:29:04 -0400

    interface w/ shapes

--- Go Fundamentals/structs/shapes.go
+++ Go Fundamentals/structs/shapes.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,10 +11,19 @@ type Circle struct {
 	Radius float64
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+// recieverVariable, recieverName, methodName
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
 }
