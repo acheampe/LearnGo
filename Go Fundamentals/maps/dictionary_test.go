3e5e88c9b4f3dfbb4a3a37967f6a049f69bbb1bf
acheampe <acheampe@oregonstate.edu>
2025-10-02 09:24:07 -0400

    refactor test

--- Go Fundamentals/maps/dictionary_test.go
+++ Go Fundamentals/maps/dictionary_test.go
@@ -5,13 +5,18 @@ import "testing"
 // maps allows for storing and retreiving values by key.
 
 func TestSearch(t *testing.T) {
-
 	dictionary := map[string]string{"test": "this is just a test"}
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
