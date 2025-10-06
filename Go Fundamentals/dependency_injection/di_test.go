365447c5c8adf9bd3fee8d78934a65e29fc48879
acheampe <acheampe@oregonstate.edu>
2025-10-06 12:27:56 -0400

    di

--- Go Fundamentals/dependency_injection/di_test.go
+++ Go Fundamentals/dependency_injection/di_test.go
@@ -1,3 +1,20 @@
 package main
 
+import (
+	"bytes" // implements writer interface
+	"testing"
+)
+
 // review structs quickly before di.
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
